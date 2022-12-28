d44c1e0b6eef4a02477a223532a59ae9bb9907cc
Sotatek-ThanhVu <thanh.vu@sotatek.com>
2022-12-28 16:03:34 +0700

    feat: add fetch button

--- packages/snap/src/index.ts
+++ packages/snap/src/index.ts
@@ -1,11 +1,4 @@
 import { OnRpcRequestHandler } from '@metamask/snap-types';
-import {
-  getBIP44AddressKeyDeriver,
-  JsonBIP44CoinTypeNode,
-} from '@metamask/key-tree';
-import converter from 'bech32-converting';
-import blake from 'blakejs';
-import { BaseAddress, NetworkInfo, StakeCredential } from '@emurgo/cardano-serialization-lib-browser';
 
 /**
  * Get a message from the origin. For demonstration purposes only.
@@ -33,62 +26,19 @@ export const onRpcRequest: OnRpcRequestHandler = async ({
 }) => {
   switch (request.method) {
     case 'hello': {
-      const derivationPath = "m/44'/1815'/0'/0/0";
-      const [, , coinType, account, change, addressIndex] =
-        derivationPath.split('/');
-      const bip44Code = Number(coinType.replace("'", ''));
-      try {
-        const bip44Node = (await wallet.request({
-          method: 'snap_getBip44Entropy',
-          params: {
-            coinType: bip44Code,
-          },
-        })) as JsonBIP44CoinTypeNode;
+      const response = await fetch(
+        'https://min-api.cryptocompare.com/data/price?fsym=USDT&tsyms=USD',
+        {
+          method: 'GET',
+        },
+      );
 
-        const addressKeyDeriver = await getBIP44AddressKeyDeriver(bip44Node, {
-          account: Number(account.replace("'", '')),
-          change: Number(change),
-        });
-        const addressPrefix = 'addr';
-
-        const addressKey = await addressKeyDeriver(Number(addressIndex));
-        // `echo "${addressKey}" | xxd -r -p - | b2sum --length 224 --binary | cut -c 1-56`
-        const cardanoAddr = converter(addressPrefix).toBech32(
-          '6186e9653a7a16716fdc286953e2e8132aeff0cabd5dd9eeee6115c00b',
-        );
-
-        // const utxoPubKey = addressKey.
-        const baseAddress = BaseAddress.new(NetworkInfo.mainnet().network_id(), StakeCredential.from_keyhash())
-        const cardanoDecoded = converter(addressPrefix).toHex(cardanoAddr);
-        console.log(
-          `-addressKeyDeriver:`,
-          Object.keys(addressKey),
-          blake.blake2bHex(addressKey.address),
-          cardanoAddr,
-          cardanoDecoded,
-        );
-
-        return wallet.request({
-          method: 'snap_confirm',
-          params: [
-            {
-              prompt: getMessage(origin),
-              description: 'Cardano address',
-              textAreaContent: `${addressKey.address}
-              | ${cardanoAddr}`,
-            },
-          ],
-        });
-      } catch (error) {
-        console.log(`===Err:`, error);
-      }
       return wallet.request({
         method: 'snap_confirm',
         params: [
           {
             prompt: getMessage(origin),
-            description: 'Cardano address',
-            textAreaContent: 'addressKey',
+            textAreaContent: JSON.stringify(await response.json()),
           },
         ],
       });
