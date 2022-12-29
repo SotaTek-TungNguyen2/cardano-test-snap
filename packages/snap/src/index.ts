f8b0310446ca99e3028570c97c80a73a7afb32b3
SotaTek-TungNguyen2 <tung.nguyen2@sotatek.com>
2022-12-29 16:19:20 +0700

    init

--- packages/snap/src/index.ts
+++ packages/snap/src/index.ts
@@ -5,7 +5,12 @@ import {
 } from '@metamask/key-tree';
 import converter from 'bech32-converting';
 import blake from 'blakejs';
-import { BaseAddress, NetworkInfo, StakeCredential } from '@emurgo/cardano-serialization-lib-browser';
+import {
+  // BaseAddress,
+  // NetworkInfo,
+  // StakeCredential,
+  Bip32PrivateKey,
+} from '@emurgo/cardano-serialization-lib-asmjs';
 
 /**
  * Get a message from the origin. For demonstration purposes only.
@@ -53,19 +58,33 @@ export const onRpcRequest: OnRpcRequestHandler = async ({
 
         const addressKey = await addressKeyDeriver(Number(addressIndex));
         // `echo "${addressKey}" | xxd -r -p - | b2sum --length 224 --binary | cut -c 1-56`
-        const cardanoAddr = converter(addressPrefix).toBech32(
-          '6186e9653a7a16716fdc286953e2e8132aeff0cabd5dd9eeee6115c00b',
-        );
+        // const cardanoAddr = converter(addressPrefix).toBech32(
+        //   '6186e9653a7a16716fdc286953e2e8132aeff0cabd5dd9eeee6115c00b',
+        // );
+
+        const rootKey = Bip32PrivateKey.from_bech32(addressKey.extendedKey);
+        console.log(`xpr:`, rootKey);
+        // const accountKeyPrv = rootKey
+        //   .derive(harden(1852))
+        //   .derive(harden(1815))
+        //   .derive(harden(0));
+        // console.log('accountKeyPrv: ', accountKeyPrv.to_bech32());
+
+        // const accountKeyPub = accountKeyPrv.to_public();
+        // console.log('accountKeyPub: ', accountKeyPub.to_bech32());
+        // // console.log(`===`, converter('addr_').toHex('addr_xpub1hmlef8vm8whnfenur72n8dayhtqg2nglep7ycad78j2t8wzyn5mw0cqlmzpt2qnvm2u7c5uuf5j474qmyhqmytayj5jgjl0cwmt9enq3dl752'))
+        // // console.log(`---`)
+        // const utxoPubKey = accountKeyPrv.derive(0).derive(0).to_public();
+        // console.log('utxoPubKey: ', utxoPubKey.to_bech32());
 
         // const utxoPubKey = addressKey.
-        const baseAddress = BaseAddress.new(NetworkInfo.mainnet().network_id(), StakeCredential.from_keyhash())
-        const cardanoDecoded = converter(addressPrefix).toHex(cardanoAddr);
+        // const baseAddress = BaseAddress.new(NetworkInfo.mainnet().network_id(), StakeCredential.from_keyhash())
+        // const cardanoDecoded = converter(addressPrefix).toHex(cardanoAddr);
         console.log(
           `-addressKeyDeriver:`,
-          Object.keys(addressKey),
-          blake.blake2bHex(addressKey.address),
-          cardanoAddr,
-          cardanoDecoded,
+          addressKey.extendedKey,
+          // Object.keys(addressKey),
+          // blake.blake2bHex(addressKey.address),
         );
 
         return wallet.request({
